3719c1422495d7ed460b2faa8977f54e4f17edc7
fil7er <fil7er@gmail.com>
2022-06-02 04:24:04 -0400

    Board

--- src/example.mjs
+++ src/example.mjs
@@ -1,3 +1,10 @@
-export function sum(a, b) {
-  return a + b;
+export function createBoard(size) {
+  let board = new Array;
+  for (let i = 0; i < size; i++) board.push(new Array(size));
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      board[i][j] = "b";
+    }
+  }
+  return board;
 }
--- test/example.test.mjs
+++ test/example.test.mjs
@@ -1,8 +1,21 @@
 import { expect } from "chai";
-import { sum } from "../src/example.mjs";
+import { createBoard } from "../src/example.mjs";
 
-describe("Example test fixture", () => {
-  it("Example test", () => {
-    expect(sum(1, 2)).to.equal(3);
+describe("Game of life", () => {
+  it("Board array length of 16", () => {
+    expect(createBoard(16).length).to.equal(16);
   });
+
+  it("Board with only dead cells before parsing the pattern", () => {
+    var board = createBoard(16);
+    var isClean = true;
+    for(let y=0; y< board.length; y++){
+      for(let x=0; x< board.length; x++){
+        if(board[y][x] != "b") isClean = false;
+      }
+    }
+    expect(createBoard(16).length).to.equal(16);
+  });
+
+
 });
